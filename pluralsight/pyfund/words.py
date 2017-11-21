63165c2a35d7eb293cf0923c112c9b199719a8e0
Unknown <Jdavidkahn@gmail.com>
2017-11-21 13:22:57 -0500

    rearrange2

---
+++ pluralsight/pyfund/words.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+from urllib.request import urlopen
+with urlopen('http://sixty-north.com/c/t.txt')as story:
+    story_words=[]
+    for line in story:
+        line_words=line.split()
+        for word in line_words:
+            story_words.append(word)
+
+for word in story_words:
+    print(word)
